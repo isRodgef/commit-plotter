287812ae9573de42e8dd0f29944905fc244c618d
rodgef <rodgef101@gmail.com>
2019-04-04 11:22:29 +0200

    got dates from commits

--- plotter.rb
+++ plotter.rb
@@ -1,6 +1,10 @@
  require 'git'
  require 'logger'
  
+ 
+ 
 g  = Git.open('.', :log => nil)#Logger.new(STDOUT))
 
-dates = 
+dates = g.log.map{|commit| commit.author.date.strftime("%m-%d-%y")}
+
+puts(dates)
