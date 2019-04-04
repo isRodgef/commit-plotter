99b59202c31d00a2bacb2f8142da2c036ae18a08
rodgef <rodgef101@gmail.com>
2019-04-04 11:31:23 +0200

    got date frequency

--- plotter.rb
+++ plotter.rb
@@ -7,4 +7,9 @@ g  = Git.open('.', :log => nil)#Logger.new(STDOUT))
 
 dates = g.log.map{|commit| commit.author.date.strftime("%m-%d-%y")}
 
-puts(dates)
+
+
+dates_total = Hash.new(0) 
+dates.each{|d| dates_total[d] =+ 1}
+
+puts(dates_total)
