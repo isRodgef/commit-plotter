0cfcb1de00e27511a858fcc49a0b628eb512784f
rodgef <rodgef101@gmail.com>
2019-04-04 11:43:13 +0200

    added Gemfile and removed some whitespace in plotter.rb

--- plotter.rb
+++ plotter.rb
@@ -7,8 +7,6 @@ g  = Git.open('.', :log => nil)#Logger.new(STDOUT))
 
 dates = g.log.map{|commit| commit.author.date.strftime("%m-%d-%y")}
 
-
-
 dates_total = Hash.new(0) 
 dates.each{|d| dates_total[d] =+ 1}
 
