0650ebb9214844899b96ce2e8c1a6c307d1c681e
rodgef <rodgef101@gmail.com>
2019-04-04 14:29:36 +0200

    started add gruff preparing data for plot

--- plotter.rb
+++ plotter.rb
@@ -1,13 +1,22 @@
- require 'git'
- require 'logger'
+require 'git'
+require 'logger'
+require 'gruff'
  
- 
- 
+chart =  Gruff:: Line.new 
 g  = Git.open('.', :log => nil)#Logger.new(STDOUT))
 
 dates = g.log.map{|commit| commit.author.date.strftime("%m-%d-%y")}
 
 dates_total = Hash.new(0) 
-dates.each{|d| dates_total[d] =+ 1}
+dates.each{|d| dates_total[d] +=  1}
 
-puts(dates_total)
+chart.labels = {}
+index = 0
+data
+dates_total.each do |year, freq|
+	chart.labels[index] = year
+	index += 1
+end
+
+puts(chart.labels)
+ 
