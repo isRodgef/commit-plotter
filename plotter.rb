2e3fd343583ed66b7611659cb13b9799be79616a
rodgef <rodgef101@gmail.com>
2019-04-05 09:14:13 +0200

    changed order of x-axis

--- plotter.rb
+++ plotter.rb
@@ -7,7 +7,7 @@ require 'gruff'
 chart =  Gruff:: Line.new 
 g  = Git.open(ARGV[0], :log => nil)#Logger.new(STDOUT))
 
-dates = g.log.map{|commit| commit.author.date.strftime("%m-%d-%y")}
+dates = g.log.map{|commit| commit.author.date.strftime("%m-%d-%y")}.reverse
 
 dates_total = Hash.new(0) 
 dates.each{|d| dates_total[d] +=  1}
